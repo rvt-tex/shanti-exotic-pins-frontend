0de0c3782314c29ab16e7e07c09cb0aa992f5c68
github username <github email address>
2020-06-02 22:16:59 -0400

    created exotic endpoint, add event listener to DOMcontent, create function getexotic and render data in card

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("well it better be working");
+const endPointExotics = "http://localhost:3000/exotics"
+
+document.addEventListener('DOMContentLoaded', () => {
+    getExotic()
+});
+
+function getExotic() {
+    fetch(endPointExotics)
+    .then(response => response.json())
+    .then(exotic => {
+        exotic.data.forEach(exotic => {
+            let newExotic = new Exotic(exotic, exotic.attributes)
+            document.querySelector('#exotic-container').innerHTML += newExotic.renderExoticCard()
+        });
+    });
+}
