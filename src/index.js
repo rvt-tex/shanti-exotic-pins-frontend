f3c4ca0df17211d8205c34d89ade7894a9d2a85c
github username <github email address>
2020-06-03 00:22:57 -0400

    updated exotic endpoint, add event listener to DOMcontent, create function fetchexotic and render data in card

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
-const endPointExotics = "http://localhost:3000/exotics"
+// const BASE_URL = "http://localhost:3000"
+// const EXOTICS_URL = `${BASE_URL}/exotics`
+const ExoticsEndPoint = "http://localhost:3000/exotics"
 
 document.addEventListener('DOMContentLoaded', () => {
-    getExotic()
+    fetchExotics();
 });
 
-function getExotic() {
-    fetch(endPointExotics)
+function fetchExotics() {
+    fetch(ExoticsEndPoint)
     .then(response => response.json())
-    .then(exotic => {
-        exotic.data.forEach(exotic => {
-            let newExotic = new Exotic(exotic, exotic.attributes)
-            document.querySelector('#exotic-container').innerHTML += newExotic.renderExoticCard()
-        });
-    });
+    .then(exotics => {
+        exotics.data.forEach(exotic => {
+            const exoticLayout = `<div data-id=${exotic.id}>
+            <h2>${exotic.attributes.brand.name}</h2>
+            <h4>${exotic.attributes.title}</h4>
+            <img src=${exotic.attributes.image_url} width: 800px; height: 600px;
+            <p>${exotic.attributes.description}</p>
+            </div>
+            <br><br>`;
+
+            document.querySelector('#exotic-container').innerHTML += exoticLayout;
+            
+        })
+    })
 }
+
+  
+ 
+  
